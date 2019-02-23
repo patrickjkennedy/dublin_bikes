03242507d175ddfc839b02f02d540b830e8cc971
patrickjkennedy <patrickken@gmail.com>
2019-02-23 22:06:37 +0000

    added files to fix merge

---
+++ scraper/README.md
@@ -0,0 +1,40 @@
+# JCDecaux & OpenWeatherMap API Scraper
+
+This repository contains scripts that query both the JCDecaux Dublin Bikes and OpenWeatherMap APIs and writes the response to a MySQL database. 
+Please note that the database needs to be created in advance of running the script, with the column names specified in the
+script. For further discussion around the database and the reasoning of it's column types etc., please see this
+Google Document:
+https://docs.google.com/document/d/1S93vuhNOTOamthBR9mcUg1IaO3tfUsxxOa3TP0FvkL4/edit?usp=sharing
+
+## API Key
+Before running the script yourself, please insert the correct credentials, your own API keys, and the names for your database and table.
+
+To request a JCDecaux API key head to https://developer.jcdecaux.com.
+
+For an OpenWeatherAPI key, head to https://openweathermap.org/api.
+
+## How do we get it to run every 5 minutes?
+To get the script to run every 5 minutes, you can set it to run as a cronjob on the server. To create a job, type the following in your terminal:
+```
+crontab -e 
+```
+You can then insert the following line of code:
+```
+*/5 * * * * /anaconda3/envs/scraper/bin/python /Users/patrick/dublin_bikes/scraper/scraper.py
+```
+
+Where `*/5 * * * * ` specifies that the program is to be ran every 5 minutes,
+`/anaconda3/envs/scraper/bin/python` is the Python interpreter from the Conda environment we want to use,
+And `/Users/patrick/dublin_bikes/scraper/scraper.py` is the full path to the script.
+
+## Creating a Conda environment from a requirements.txt
+First, create your Conda environment (e.g. scraper_prototype) as follows:
+
+```
+conda create -n scraper python=3.7
+```
+
+Activate your new environment with `conda activate scraper` and install the required Python packages using the command:
+```
+pip install -r requirements.txt
+```
---
+++ scraper/scraper.py
@@ -0,0 +1,79 @@
+import requests
+import mysql.connector
+import time
+import sys
+
+# Setup the database connection
+try:
+    mydb = mysql.connector.connect(
+        host="",
+        user="",
+        passwd="",
+        database="",
+        auth_plugin='mysql_native_password'
+    )
+except mysql.connector.Error as err:
+    print("Unable to connect to database: {}".format(err))
+    sys.exit(1)
+
+# Get the data from the API
+url_bikes = "https://api.jcdecaux.com/vls/v1/stations?contract=Dublin&apiKey={}"
+url_weather = 'https://api.openweathermap.org/data/2.5/weather?id=7778677&appid={}'
+
+try:
+    response_bikes = requests.get(url_bikes)
+    data_bikes = response_bikes.json()
+
+except requests.exceptions.RequestException as e:
+    print(e)
+    sys.exit(1)
+
+try:
+    response_weather = requests.get(url_weather)
+    data_weather = response_weather.json()
+
+except requests.exceptions.RequestException as e:
+    print(e)
+    sys.exit(1)
+
+# Create the insert statement for the new data
+sql_bikes = "INSERT INTO dublin_bikes_availability (number, bike_stands, available_bike_stands, " \
+            "available_bikes, status, last_update) " \
+            "VALUES (%s, %s, %s, %s, %s, %s)"
+
+sql_weather = "INSERT INTO weather (coord_lon, coord_lat, weather_id, weather_main, weather_description, "\
+            "weather_icon, base, main_temp, main_pressure, main_humidity, main_temp_min, main_temp_max, "\
+            "visibility, wind_speed, wind_deg, clouds_all, dt, sys_type, sys_id, sys_message, sys_country, "\
+            "sys_sunrise, sys_sunset, city_id, city_name, cod) "\
+            "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+
+try:
+    mycursor = mydb.cursor()
+
+# Iterate through the data response object and perform inserts
+    for elem in range(0, len(data_bikes)):
+        val = (data_bikes[elem]["number"], data_bikes[elem]["bike_stands"], data_bikes[elem]["available_bike_stands"],
+               data_bikes[elem]["available_bikes"], data_bikes[elem]["status"],
+               time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(data_bikes[elem]["last_update"]/1000)))
+
+        mycursor.execute(sql_bikes, val)
+
+    val2=(data_weather['coord']['lon'],data_weather['coord']['lat'],data_weather['weather'][0]['id'],data_weather['weather'][0]['main'],
+          data_weather['weather'][0]['description'],data_weather['weather'][0]['icon'],data_weather['base'],data_weather['main']['temp'],
+          data_weather['main']['pressure'],data_weather['main']['humidity'],data_weather['main']['temp_min'],data_weather['main']['temp_max'],
+          data_weather['visibility'],data_weather['wind']['speed'],data_weather['wind']['deg'],data_weather['clouds']['all'],
+          time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(data_weather['dt'])),data_weather['sys']['type'],
+          data_weather['sys']['id'],data_weather['sys']['message'],data_weather['sys']['country'],
+          time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(data_weather['sys']['sunrise'])),
+          time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(data_weather['sys']['sunset'])),data_weather['id'],data_weather['name'],data_weather['cod'])
+
+    mycursor.execute(sql_weather, val2)
+
+    mydb.commit()
+
+    # Close the connection
+    mydb.close()
+
+except mysql.connector.Error as err:
+    print("Unable to connect to database: {}".format(err))
+    sys.exit(1)

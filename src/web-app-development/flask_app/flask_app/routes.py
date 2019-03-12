91d1019184270ed1d5d1671a2a57f61cee202ef4
Kieran Curtin <kieran.curtin@ucdconnect.ie>
2019-03-12 20:05:37 +0000

    minor changes made to flask app and templates for app to work when cloned.  Updated map.html -  added dropdown menus and some CSS styling.

--- src/web-app-development/flask_app/flask_app/routes.py
+++ src/web-app-development/flask_app/flask_app/routes.py
@@ -5,23 +5,7 @@ import os
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Southwark'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        },
-        {
-            'author': {'username': 'Caesar Salad'},
-            'body': 'Yum, these dublin bikes taste delicious!'
-        }
-    ]
-
-    filename = os.path.join(app.static_folder, 'static.json')
-    with open(filename) as bikes:
-        data = json.load(bikes)
-    return render_template('index.html', title='Home', user=user, posts=posts, data=data)
+    filename = os.path.join(app.static_folder, 'placeholder.json')
+    with open(filename) as placeholder_data:
+        data = json.load(placeholder_data)
+    return render_template('index.html', title='Home', data=data)

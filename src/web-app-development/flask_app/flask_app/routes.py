226956332ec8d106ef8e06a97604ba4c719b676a
Kieran Curtin <kieran.curtin@ucdconnect.ie>
2019-03-13 13:33:40 +0000

    security update - updated routes.py and mapp.html for passing maps url+api from flask

--- src/web-app-development/flask_app/flask_app/routes.py
+++ src/web-app-development/flask_app/flask_app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, json
 from flask_app import app
 import os
+import config
 
 @app.route('/')
 @app.route('/index')
@@ -8,4 +9,4 @@ def index():
     filename = os.path.join(app.static_folder, 'placeholder.json')
     with open(filename) as placeholder_data:
         data = json.load(placeholder_data)
-    return render_template('index.html', title='Home', data=data)
+    return render_template('index.html', title='Home', data=data, map_key=config.map_url)

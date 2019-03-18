39b0da87b4ffabf292ffc5cae58ab1568823e71a
Kieran Curtin <kieran.curtin@ucdconnect.ie>
2019-03-18 14:29:59 +0000

    integrated backend and frontend - frontend now revieving information from SQL queries and not API calls

--- src/web-app-development/flask_app/flask_app/routes.py
+++ src/web-app-development/flask_app/flask_app/routes.py
@@ -35,16 +35,24 @@ def close_connection(exception):
 @app.route('/')
 @app.route('/index')
 def index():
-    filename = os.path.join(app.static_folder, 'placeholder.json')
-    with open(filename) as placeholder_data:
-        data = json.load(placeholder_data)
-    return render_template('index.html', title='Home', data=data, map_key=config.map_url)
+    result_stations = get_stations()
+    result_current_availability = get_current_availability()
+    data_stations = json.loads(result_stations)
+    data_current_availability = json.loads(result_current_availability)
+    return render_template('index.html', title='Home', data_stations=data_stations, data_current_availability=data_current_availability, map_key=config.map_url)
 
-@app.route("/stations")
+@app.route("/api/stations")
 def get_stations():
     connection = get_db()
     with connection.cursor() as cursor:
-        cursor = connection.cursor()
         cursor.execute("SELECT * FROM dublin_bikes_static")
+        data = cursor.fetchall()
+    return json.dumps(data, default=str)
+
+@app.route("/api/current_availability")
+def get_current_availability():
+    connection = get_db()
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM scraper.dublin_bikes_availability order by id desc limit 113;")
         data = cursor.fetchall()
     return json.dumps(data, default=str)

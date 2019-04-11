ed5c6033cf925a1af029fab1ccd8cb19e577a18f
Kieran Curtin <kieran.curtin@ucdconnect.ie>
2019-04-11 18:37:06 +0100

    added tests - resolved POST/GET

--- tests.py
+++ tests.py
@@ -23,6 +23,14 @@ class DublinBikesUnitTests(unittest.TestCase):
     def test_forecast(self):
         response = self.application.get('/api/forecast').get_json()
         self.assertEqual(len(response),8)
+    
+    def test_user_input(self):
+        response = self.application.get('/user_input?StationselectFrom=2&SelectcollectTime=18%3A20&StationselectTo=2&SelectdropTime=18%3A50').get_json()
+        self.assertEqual(response['from_station'], "2")
+
+    def test_current_weather(self):
+        response = self.application.get('api/current_weather').get_json()
+        self.assertEqual(len(response), 1)
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
